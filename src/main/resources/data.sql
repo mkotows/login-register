11f2d39efa9cbbf9ec2cbe7774f9d1ef4dd8b5bc
Mateusz Kotowski <kotas55@wp.pl>
2018-06-04 08:47:57 +0200

    database changed to spring_security

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
 
-insert into users(username, password, enabled)values('admin','{noop}admin',true);
+insert into users(username, password, enabled)values('admin','$2a$10$WaEoJRkZXi4R44CMP0URMufyHo37swupsSMzYJ/icyjcAaLnof.HK',true);
 insert into authorities(username,authority)values('admin','ROLE_ADMIN');
 
-insert into users(username, password, enabled)values('employee','{noop}employee',true);
-insert into authorities(username,authority)values('employee','ROLE_USER');
+-- insert into users(username, password, enabled)values('employee','{noop}employee',true);
+-- insert into authorities(username,authority)values('employee','ROLE_USER');
 
 -- insert into employee(name)values('pierwszy');
